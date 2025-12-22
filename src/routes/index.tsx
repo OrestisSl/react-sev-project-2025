8d2fed42059a8792826c02a41d96d5ff567d5e57
Orestis <orestisgamer23@gmail.com>
2025-12-22 19:14:01 +0200

    feat(layout): add MainLayout and nest routes under shared layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Home , CreateRecipe , Preferences } from "../pages";
+import { Home, CreateRecipe, Preferences } from "../pages";
+import MainLayout from "../layouts/MainLayout";
 
 export const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home/>,
+    element: <MainLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/create-recipe", element: <CreateRecipe /> },
+      { path: "/preferences", element: <Preferences /> },
+    ],
   },
-  {
-    path: "/create-recipe",
-    element: <CreateRecipe/>,
-  },
-  {
-    path: "/preferences",
-    element: <Preferences />
-  }
 ]);
