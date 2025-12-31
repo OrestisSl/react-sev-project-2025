0b0f2cb3ea8c1e46f2483907754d347ee37c48ef
Orestis <orestisgamer23@gmail.com>
2025-12-31 16:37:52 +0200

    chore(types): add servings property to recipe card prop interface

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,6 +3,7 @@ export type RecipeDifficulty = "Easy" | "Medium" | "Hard";
 export interface RecipeCardProp {
   title: string;
   image: string;
-  time: string;
+  time: number;
+  servings?: number;
   difficulty: RecipeDifficulty;
 }
